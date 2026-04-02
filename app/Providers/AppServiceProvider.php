59daa0ac4a300d330f5b1a16612db08c91d81eb6
Linh Nguyen <linh.nguyen@kamora.vn>
2026-04-02 16:55:21 +0700

    Remove unnecessary module Post + Document

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Knuckles\Scribe\Scribe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scribe::afterGenerating(function (array $paths) {
+            if (! empty($paths['postman']) && file_exists($paths['postman'])) {
+                $json = json_decode(file_get_contents($paths['postman']), true);
+                file_put_contents(
+                    $paths['postman'],
+                    json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+                );
+            }
+        });
     }
 }
